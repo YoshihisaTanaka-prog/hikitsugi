76acc66a59bd43af3b6f6a4e3af92bf0bafdd56f
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-02-26 11:51:47 +0900

    校舎の編集

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,8 @@
 class School < ApplicationRecord
+  
+  validates :name, presence: :true
+  validates :school_id, presence: :true
+  validates :prefecture_id, presence: :true
 
   def hash_format
     return {id: self.id, name: self.name}
