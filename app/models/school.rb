b5de1bc877777dd6cd8ca4577ee1ce5b15427918
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-02-26 11:54:18 +0900

    Update school.rb

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,6 +3,8 @@ class School < ApplicationRecord
   validates :name, presence: :true
   validates :school_id, presence: :true
   validates :prefecture_id, presence: :true
+  validates :name, uniqueness: true
+  validates :school_id, uniqueness: :true
 
   def hash_format
     return {id: self.id, name: self.name}
