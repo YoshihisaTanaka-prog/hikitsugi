8525bfecbe1be36a19f9ebdb050bf65f1d21ce6c
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-02-27 17:18:31 +0900

    update for assets

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Hikitsugi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.prefix = '/apps/assets'
   end
 end
